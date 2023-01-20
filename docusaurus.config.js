2a343735bcddf74d5352cbe4e46e358f35157109
Yuchen Wang <yuchenwang@apple.com>
2023-01-19 17:27:57 -0800

    baseURL change

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -9,7 +9,7 @@ const config = {
   title: 'My Site',
   tagline: 'Dinosaurs are cool',
   url: 'https://your-docusaurus-test-site.com',
-  baseUrl: '/',
+  baseUrl: '/ECUST-Leap-Handbook/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
