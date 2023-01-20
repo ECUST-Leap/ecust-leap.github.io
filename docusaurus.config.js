57b89168c5f926b6535f97c393c29283fd687c17
Yuchen Wang <yuchenwang@apple.com>
2023-01-19 16:59:54 -0800

    config changes

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -16,8 +16,10 @@ const config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'Yuchen-Wang-SH', // Usually your GitHub org/user name.
+  projectName: 'ECUST-Leap-Handbook', // Usually your repo name.
+  deploymentBranch: 'main',
+  trailingSlash: false,
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
