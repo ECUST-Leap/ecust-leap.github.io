c4c7960abf32fabff5aba256aa065b0b75dcd917
Yuchen Wang <yuchenwang@apple.com>
2023-02-05 01:56:45 -0800

    copied from doc comment feature

--- src/theme/BlogPostItem/index.tsx
+++ src/theme/BlogPostItem/index.tsx
@@ -2,13 +2,30 @@ import React from 'react';
 import BlogPostItem from '@theme-original/BlogPostItem';
 import type BlogPostItemType from '@theme/BlogPostItem';
 import type {WrapperProps} from '@docusaurus/types';
+import { useColorMode } from "@docusaurus/theme-common";
+import Giscus from "@giscus/react";
 
 type Props = WrapperProps<typeof BlogPostItemType>;
 
 export default function BlogPostItemWrapper(props: Props): JSX.Element {
+  const { colorMode } = useColorMode();
   return (
     <>
       <BlogPostItem {...props} />
+      <Giscus
+        repo="ECUST-Leap/ecust-leap.github.io"
+        repoId="R_kgDOIzvQtQ"
+        category="评论区"
+        categoryId="DIC_kwDOIzvQtc4CT-SP"
+        mapping="url"
+        strict="1"
+        reactionsEnabled="1"
+        emitMetadata="1"
+        inputPosition="top"
+        theme={colorMode}
+        lang="zh-CN"
+        loading="lazy"
+      />
     </>
   );
 }
