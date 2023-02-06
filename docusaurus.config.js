51e403638b9a3d5a56dcec16b81596aa86c5557e
Yuchen Wang <yuchenwang@apple.com>
2023-02-05 17:24:00 -0800

    variable extraction

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -4,12 +4,14 @@
 const lightCodeTheme = require('prism-react-renderer/themes/github');
 const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
+const baseUrl = process.env.BASE_URL ?? '/';
+
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: '华东理工大学飞跃手册',
   tagline: 'Dinosaurs are cool',
   url: 'https://your-docusaurus-test-site.com',
-  baseUrl: process.env.BASE_URL ?? '/',
+  baseUrl: baseUrl,
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
@@ -67,7 +69,7 @@ const config = {
       // @ts-ignore
       ({
         hashed: true,
-        docsRouteBasePath: process.env.BASE_URL ?? '/',
+        docsRouteBasePath: baseUrl,
         language: ["en", "zh"],
         highlightSearchTermsOnTargetPage: true,
       }),
