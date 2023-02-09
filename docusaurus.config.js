59de872d3c5346a8f46daaeb0555432287316683
Yuchen Wang <yuchenwang@apple.com>
2023-02-08 17:37:50 -0800

    up left logo

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -83,7 +83,7 @@ const config = {
         title: '华理飞跃',
         logo: {
           alt: 'My Site Logo',
-          src: 'img/logo.svg',
+          src: 'img/logo.png',
         },
         items: [
           {
