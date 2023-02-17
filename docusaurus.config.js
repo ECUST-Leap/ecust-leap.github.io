126c2c4190ce381cf2050dc05b9bcdac75f09466
Yuchen Wang <yuchenwang@apple.com>
2023-02-17 01:06:31 -0800

    github icon

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -100,8 +100,9 @@ const config = {
           },
           {
             href: 'https://github.com/ECUST-Leap/ecust-leap.github.io',
-            label: 'GitHub',
             position: 'right',
+            className: "header-github-link",
+            "aria-label": "GitHub repository",
           },
         ],
       },
